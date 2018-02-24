2a6c53fdff53c1cea816631995cd610950be341d
Michael Hall <mbhall88@gmail.com>
2018-02-24 12:05:21 +0000

    add test for quality per position plots

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -45,7 +45,7 @@ def test_length_vs_qual_plot():
     """Test generation of read length vs. quality score plot."""
     fname = os.path.join(IMG_DIR, 'len_v_qual.png')
     expected_fname = os.path.join(IMG_DIR, 'len_v_qual-expected.png')
-    
+
     lengths, quality_scores = get_test_data()[1:3]
     fig = plots.length_vs_qual_plot(lengths, quality_scores)
     fig.savefig(fname, format='png')
@@ -53,4 +53,20 @@ def test_length_vs_qual_plot():
 
 
 def test_quality_per_position():
-    pass
+    """Test generation of the quality per position plots."""
+    fname_start = os.path.join(IMG_DIR, 'qual_pos_start.png')
+    expected_fname_start = os.path.join(IMG_DIR, 'qual_pos_start-expected.png')
+    fname_end = os.path.join(IMG_DIR, 'qual_pos_end.png')
+    expected_fname_end = os.path.join(IMG_DIR, 'qual_pos_end-expected.png')
+
+    df_start, df_end = get_test_data()[3:]
+    fig_start = plots.quality_per_position(df_start)
+    fig_end = plots.quality_per_position(df_end, from_end='end')
+    fig_start.savefig(fname_start, format='png')
+    fig_end.savefig(fname_end, format='png')
+    assert (open(expected_fname_start, 'rb').read() ==
+            open(fname_start, 'rb').read())
+    assert (open(expected_fname_end, 'rb').read() ==
+            open(fname_end, 'rb').read())
+
+
